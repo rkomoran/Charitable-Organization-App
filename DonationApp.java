912266e2e8d5339ac97688f3eaadada4f284ad04
hannah-sarty <z376s@unb.ca>
2025-11-16 15:09:26 -0400

    Update DonationApp.java

--- DonationApp.java
+++ DonationApp.java
@@ -79,6 +79,7 @@ public class DonationApp extends Application {
 				"Your donations make real impact in our community."
         );
         info.setWrapText(true);
+		info.setMaxWidth(660);
 
         ProgressBar bar = new ProgressBar(ratio(total));
         Label raised;
